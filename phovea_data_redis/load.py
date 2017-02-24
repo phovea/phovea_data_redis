d0936a69c341f3c019ca8255f5b54c07a8e27a93
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-24 11:53:48 +0100

    refactor mapping manager, redis is just a provider for mappings

--- phovea_data_redis/load.py
+++ phovea_data_redis/load.py
@@ -50,6 +50,7 @@ def load_ids_from_file(idtype, file_name, set_max=True):
 def load_mapping_from_file(from_idtype, to_idtype, file_name):
   db = _get_mapping_db()
   _log.info('loading mapping from %s -> %s from %s', from_idtype, to_idtype, file_name)
+  db.set('mappingFrom{}2{}'.format(from_idtype, to_idtype), to_idtype)
   with open(file_name, 'r', encoding='utf-8') as f:
     for line in f:
       [from_id, to_id] = line.split('\t')
--- phovea_data_redis/mapping_table.py
+++ phovea_data_redis/mapping_table.py
@@ -1,36 +1,55 @@
-def ascii(s):
-  return s
+def create_db():
+  import redis
+  import phovea_server.config
+
+  c = phovea_server.config.view('phovea_data_redis.mapping')
+
+  # print c.hostname, c.port, c.db
+  return redis.Redis(host=c.hostname, port=c.port, db=c.db)
 
 
 class RedisMappingTable(object):
-  """
-  assigns ids to object using a redis database
-  """
+  def __init__(self, from_idtype, to_idtype):
+    self.from_idtype = from_idtype
+    self.to_idtype = to_idtype
+    self._map = None
 
+  def _load(self):
+    from itertools import izip
+    db = create_db()
+    prefix = '{}2{}.'.format(self.from_idtype, self.to_idtype)
+    keys = [k for k in db.scan_iter(match=prefix + '*')]
+    values = db.mget(keys)
+
+    self._map = dict()
+    prefix_length = len(prefix)
+    for key, value in izip(keys, values):
+      from_id = key[prefix_length + 1:]
+      self._map[from_id] = value.split(';')
+
+  def __call__(self, ids):
+    if not self._map:
+      self._load()
+
+    return [self._map.get(id, None) for id in ids]
+
+
+def _discover_mappings():
+  db = create_db()
+  keys = [k for k in db.scan_iter(match='mappingFrom*')]
+  for key in keys:
+    key = key[len('mappingFrom') + 1:]
+    parts = key.split('2')
+    yield RedisMappingTable(parts[0], parts[1])
+
+
+class RedisMappingProvider(object):
   def __init__(self):
-    import redis
-    import phovea_server.config
+    self._mappings = list(_discover_mappings())
 
-    c = phovea_server.config.view('phovea_data_redis.mapping')
-
-    # print c.hostname, c.port, c.db
-    self._db = redis.Redis(host=c.hostname, port=c.port, db=c.db)
-
-  def __call__(self, from_idtype, to_idtype, ids):
-    from_idtype = ascii(from_idtype)
-    to_idtype = ascii(to_idtype)
-
-    key = from_idtype + '2' + to_idtype
-
-    def lookup(id):
-      fkey = key + '.' + ascii(id)
-      r = self._db.get(fkey)
-      if r is None:
-        return None
-      return r.split(';')
-
-    return map(lookup, ids)
+  def __iter__(self):
+    return iter(self._mappings)
 
 
 def create():
-  return RedisMappingTable()
+  return RedisMappingProvider()
