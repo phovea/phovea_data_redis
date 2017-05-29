145fd074a092de7c784c9d63bc7f525b2df5d5a2
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-05-29 09:24:56 +0200

    option to preload mapping tables during startup

--- phovea_data_redis/mapping_table.py
+++ phovea_data_redis/mapping_table.py
@@ -4,11 +4,15 @@ from itertools import izip, islice
 _log = logging.getLogger(__name__)
 
 
+def _get_config():
+  import phovea_server.config
+  return phovea_server.config.view('phovea_data_redis.mapping')
+
+
 def create_db():
   import redis
-  import phovea_server.config
 
-  c = phovea_server.config.view('phovea_data_redis.mapping')
+  c = _get_config()
 
   # print c.hostname, c.port, c.db
   return redis.Redis(host=c.hostname, port=c.port, db=c.db)
@@ -45,6 +49,40 @@ class RedisMappingTable(object):
     return [dict(match=key[len(prefix):], to=value) for key, value in izip(keys, values)]
 
 
+class CachedRedisMappingTable(object):
+  def __init__(self, from_idtype, to_idtype, db):
+    self.from_idtype = from_idtype
+    self.to_idtype = to_idtype
+    self._cache = self._load_cache(db)
+
+  def _load_cache(self, db):
+    prefix = '{}2{}.'.format(self.from_idtype, self.to_idtype)
+    match = prefix + '*'
+    keys = [k for k in db.scan_iter(match=match)]
+    values = db.mget(keys)
+    return {key[len(prefix):]: value for key, value in izip(keys, values)}
+
+  def __call__(self, ids):
+    def map_impl(id):
+      v = self._cache.get(id, '')
+      return v.split(';')
+
+    return [map_impl(id) for id in ids]
+
+  def search(self, query, max_results=None):
+    import fnmatch
+    """
+    searches for matches in the names of the given idtype
+    :param query:
+    :param max_results
+    :return:
+    """
+    query = query.lower()
+    pattern = '*{}*'.format(query)
+    return [dict(match=key, to=self._cache[key]) for key in
+            islice(fnmatch.filter(self._cache.keys(), pattern), max_results)]
+
+
 def _discover_mappings():
   db = create_db()
   mappings = db.get('mappings')
@@ -52,10 +90,13 @@ def _discover_mappings():
   if not mappings:
     return
   mappings = [r for r in mappings.split(';') if r.strip()]
+  cached = _get_config().bulk
   for key in mappings:
     parts = key.split('2')
     _log.info('loading redis mapping table from %s to %s', parts[0], parts[1])
-    yield RedisMappingTable(parts[0], parts[1])
+    from_idtype = parts[0]
+    to_idtype = parts[1]
+    yield (CachedRedisMappingTable(from_idtype, to_idtype, db) if cached else RedisMappingTable(from_idtype, to_idtype))
 
 
 class RedisMappingProvider(object):
